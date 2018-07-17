8d5a9a95c2130b0e1f75ccb26e69a7ac03a2f741
jrandiny <jrandiny2@gmail.com>
2018-07-17 18:15:23 +0700

    Better setup config saving

--- Desktop/setup.py
+++ Desktop/setup.py
@@ -5,17 +5,19 @@ import requests
 import argparse
 
 def generate_key():
-  return os.urandom(40).encode('hex')
+  return os.urandom(40).encode("hex")
 
 # Argument
-parser = argparse.ArgumentParser(description='Setup pam-android-fingerprint')
+parser = argparse.ArgumentParser(description="Setup pam-android-fingerprint")
 parser.add_argument("-f","--first", action = "store_true", help="Setting up for the first time")
-parser.add_argument("-v", "--version", action='version', version='pam-android-fingerprint 0.1')
+parser.add_argument("-v", "--version", action="version", version="pam-android-fingerprint 0.1")
 
 args = parser.parse_args()
 
 if args.first:
   print "Setting up for the first time"
+  if os.path.isfile("config"):
+    os.remove("config")
 
 # Create config
 config = ConfigParser.ConfigParser()
@@ -35,9 +37,11 @@ while True:
   except:
     print "URL Invalid"
 
-config.read('config')
-config.add_section('SERVER')
-config.set('SERVER', 'url' , server_url)
+config.read("config")
+if(not(config.has_section("SERVER"))):
+  config.add_section("SERVER")
+
+config.set("SERVER", "url" , server_url)
 
 # setup secret
 if args.first:
@@ -45,10 +49,8 @@ if args.first:
   queryParam = {"secret":secretKey}
   r = requests.post(server_url+"/store",params=queryParam)
 
-  print "abc"
-
-  config.add_section('SECRET')
-  config.set('SECRET', 'secret' , secretKey)
+  config.add_section("SECRET")
+  config.set("SECRET", "secret" , secretKey)
 
 # write config
-config.write(open('config', 'w'))
+config.write(open("config", "w"))
