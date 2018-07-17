c991f5c21a2cd013bc014e2e3c25e211ccc69d49
jrandiny <jrandiny2@gmail.com>
2018-07-18 00:12:24 +0700

    Add fab to main activity

--- Android/app/src/main/java/com/randiny_games/fingerprintauthenticator/MainActivity.java
+++ Android/app/src/main/java/com/randiny_games/fingerprintauthenticator/MainActivity.java
@@ -1,24 +1,26 @@
 package com.randiny_games.fingerprintauthenticator;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.text.format.Formatter;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.IOException;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private MyHTTPD server;
     private TextView statusText;
     private String ip;
+    private FloatingActionButton fab;
+    private Intent serverIntent;
+    private Button setupButton;
+
+    private Boolean serverStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,26 +28,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         statusText = (TextView)findViewById(R.id.statusText);
-        statusText.setText(getString(R.string.starting));
+        fab = (FloatingActionButton) findViewById(R.id.startStopFab);
+        setupButton = (Button) findViewById(R.id.setupBtn);
 
-        Intent intent = new Intent(this, server.class);
-        startService(intent);
+        serverIntent = new Intent(this, server.class);
 
-        statusText.setText(getString(R.string.started));
+        if(savedInstanceState!=null){
+            serverStatus = savedInstanceState.getBoolean("serverStatus");
+            if(!serverStatus){
+                startServer();
+            }
+        }else {
+            startServer();
+        }
 
-        findViewById(R.id.setupBtn).setOnClickListener(this);
+        fab.setOnClickListener(this);
+        setupButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        outState.putBoolean("serverStatus",serverStatus);
+        super.onSaveInstanceState(outState, outPersistentState);
+
+    }
+
+    private void startServer(){
+        statusText.setText(getString(R.string.statusStarting));
+
+        startService(serverIntent);
+        fab.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_stop_white_24dp));
+        serverStatus = true;
 
         WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
 
-        statusText.setText(getString(R.string.started) + " on " + ip);
+        statusText.setText(getString(R.string.statusStarted) + ip);
+    }
 
+    private void stopServer(){
+        stopService(serverIntent);
+        fab.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_play_arrow_white_24dp));
+        serverStatus = false;
+
+        statusText.setText(R.string.statusStopped);
     }
 
 
     @Override
     public void onClick(View view) {
-        if(view.getId() == R.id.setupBtn){
+        if(view.getId() == R.id.startStopFab){
+            if(serverStatus){
+                stopServer();
+            }else {
+                startServer();
+            }
 
         }
     }
