1b39cdb2faf85537406f4c73a4391d5e86cd6206
jrandiny <jrandiny2@gmail.com>
2018-07-24 21:11:48 +0700

    Fix styling and faster timeout

--- Desktop/main.py
+++ Desktop/main.py
@@ -7,47 +7,69 @@ import getpass
 DEFAULT_USER = "nobody"
 CONFIG_LOC = "/home/joshua/Dev/pam-android-fingerprint/Desktop/config"
 
+
 def pam_sm_authenticate(pamh, flags, argv):
+    print "Preparing to authenticate using fingerprint"
 
-  print "Preparing to authenticate using fingerprint"
+    config = ConfigParser.ConfigParser()
+    config.readfp(open(CONFIG_LOC, 'r'))
+    server_url = config.get('SERVER', 'url')
+    secret = config.get('SECRET', 'secret')
 
-  config = ConfigParser.ConfigParser()
-  config.readfp(open(CONFIG_LOC,'r'))
-  server_url = config.get('SERVER', 'url')
-  secret = config.get('SECRET', 'secret')
+    try:
+        user = pamh.get_user(None)
+    except pamh.exception, e:
+        return e.pam_result
 
-  try:
-    user = pamh.get_user(None)
-  except pamh.exception, e:
-    return e.pam_result
-  if user == None:
-    pamh.user = DEFAULT_USER
+    if user == None:
+        pamh.user = DEFAULT_USER
 
-  totp = pyotp.TOTP(secret)
+    totp = pyotp.TOTP(secret)
 
-  print "Requesting fingerprint from " + server_url
+    print "Requesting fingerprint from " + server_url
 
-  r = requests.get(server_url + "/token",timeout=30)
-  content = r.json();
+    try:
+        r = requests.get(server_url + "/identity", timeout=1)
+        content = r.json()
 
-  if totp.verify(content["token"]):
-    print "Authenticated using fingerprint"
-    return pamh.PAM_SUCCESS
-  else:
-    print "Authentication failed"
-    return pamh.PAM_AUTH_ERR
+    except:
+        print "Phone not connected"
+        return pamh.PAM_AUTH_ERR
+
+    if content["identity"] != "Fingerprint Server":
+        print "Phone not connected"
+        return pamh.PAM_AUTH_ERR
+
+    try:
+        r = requests.get(server_url + "/token", timeout=30)
+        content = r.json()
+    except:
+        print "Connection error"
+        return pamh.PAM_AUTH_ERR
+
+    if totp.verify(content["token"]):
+        print "Authenticated using fingerprint"
+        return pamh.PAM_SUCCESS
+    else:
+        print "Authentication failed"
+        return pamh.PAM_AUTH_ERR
+
 
 def pam_sm_setcred(pamh, flags, argv):
-  return pamh.PAM_SUCCESS
+    return pamh.PAM_SUCCESS
+
 
 def pam_sm_acct_mgmt(pamh, flags, argv):
-  return pamh.PAM_SUCCESS
+    return pamh.PAM_SUCCESS
+
 
 def pam_sm_open_session(pamh, flags, argv):
-  return pamh.PAM_SUCCESS
+    return pamh.PAM_SUCCESS
+
 
 def pam_sm_close_session(pamh, flags, argv):
-  return pamh.PAM_SUCCESS
+    return pamh.PAM_SUCCESS
+
 
 def pam_sm_chauthtok(pamh, flags, argv):
-  return pamh.PAM_SUCCESS
+    return pamh.PAM_SUCCESS
--- Desktop/setup.py
+++ Desktop/setup.py
@@ -5,67 +5,70 @@ import requests
 import argparse
 import base64
 
+
 def generate_key():
-  return base64.b32encode(os.urandom(40))
+    return base64.b32encode(os.urandom(40))
+
 
 # Argument
 parser = argparse.ArgumentParser(description="Setup pam-android-fingerprint")
-parser.add_argument("-f","--first", action = "store_true", help="Setting up for the first time")
+parser.add_argument("-f", "--first", action="store_true", help="Setting up for the first time")
 parser.add_argument("-v", "--version", action="version", version="pam-android-fingerprint 0.1")
 
 args = parser.parse_args()
 
 if args.first:
-  print "Setting up for the first time"
-  if os.path.isfile("config"):
-    os.remove("config")
+    print "Setting up for the first time"
+    if os.path.isfile("config"):
+        os.remove("config")
 
 # Create config
 config = ConfigParser.ConfigParser()
 
 # Ask input server
 while True:
-  server_url = raw_input("Server location : ")
+    server_url = raw_input("Server location : ")
 
-  #Make lower
-  server_url = server_url.lower()
+    # Make lower
+    server_url = server_url.lower()
 
-  # check if port exist
-  if not ":" in server_url:
-    server_port = raw_input("Server port : ")
-    server_url = server_url + ":" + server_port
+    # check if port exist
+    if not ":" in server_url:
+        server_port = raw_input("Server port : ")
+        server_url = server_url + ":" + server_port
 
-  # Add http
-  if(server_url[:4] != "http"):
-    server_url = "http://" + server_url
+    # Add http
+    if (server_url[:4] != "http"):
+        server_url = "http://" + server_url
 
-  # Check
-  try:
-    r = requests.get(server_url + "/identity")
-    content = r.json();
-    if content["identity"] == "Fingerprint Server":
-      break
-    else:
-      print "URL Invalid"
-  except:
-    print "URL Invalid"
+    # Check
+    try:
+        r = requests.get(server_url + "/identity")
+        content = r.json();
+        if content["identity"] == "Fingerprint Server":
+            break
+        else:
+            print "URL Invalid"
+    except:
+        print "URL Invalid"
 
 config.read("config")
-if(not(config.has_section("SERVER"))):
-  config.add_section("SERVER")
+if (not (config.has_section("SERVER"))):
+    config.add_section("SERVER")
 
-config.set("SERVER", "url" , server_url)
+config.set("SERVER", "url", server_url)
 
 # setup secret
 if args.first:
-  secretKey = generate_key()
-  queryParam = {"secret":secretKey}
-  r = requests.post(server_url+"/store",params=queryParam,timeout=30)
+    secretKey = generate_key()
+    queryParam = {"secret": secretKey}
 
-  print "Verify on phone"
+    print "Verify on phone"
 
-  config.add_section("SECRET")
-  config.set("SECRET", "secret" , secretKey)
+    r = requests.post(server_url + "/store", params=queryParam, timeout=30)
+
+    config.add_section("SECRET")
+    config.set("SECRET", "secret", secretKey)
 
 # write config
 config.write(open("config", "w"))
